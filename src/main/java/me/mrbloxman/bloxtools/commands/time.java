fa419ed1596c41cf5972ba0e3268be95674e5103
RandomKittenCat <75283280+RandomKittenCat@users.noreply.github.com>
2024-03-14 21:04:20 -0700

    time and weather fix

--- src/main/java/me/mrbloxman/bloxtools/commands/time.java
+++ src/main/java/me/mrbloxman/bloxtools/commands/time.java
@@ -15,9 +15,16 @@ public class time implements CommandExecutor {
         if (sender instanceof Player p) {
             if (args.length == 0) {
                 p.sendMessage(ChatColor.RED + "Please specify the time");
-            }else{
-                p.performCommand("time set " + Arrays.toString(args));
-                p.sendMessage(ChatColor.YELLOW + "Time set to " + Arrays.toString(args));
+            }
+
+            String timeArg = args[0];
+
+            try{
+                long timeValue = Long.parseLong(timeArg);
+                p.getWorld().setTime(timeValue);
+                p.sendMessage(ChatColor.YELLOW + "Time set to " + timeValue);
+            } catch (NumberFormatException e) {
+                p.sendMessage(ChatColor.RED + "Invalid time format, please specify a number");
             }
         }
         return true;
--- src/main/java/me/mrbloxman/bloxtools/commands/weather.java
+++ src/main/java/me/mrbloxman/bloxtools/commands/weather.java
@@ -1,6 +1,7 @@
 package me.mrbloxman.bloxtools.commands;
 
 import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,16 +9,35 @@ import org.bukkit.entity.Player;
 
 import javax.annotation.Nonnull;
 import java.util.Arrays;
+import java.util.Locale;
 
 public class weather implements CommandExecutor {
     @Override
     public boolean onCommand(@Nonnull CommandSender sender,@Nonnull Command command,@Nonnull String s,@Nonnull String[] args) {
         if (sender instanceof Player p) {
             if (args.length == 0) {
-                p.sendMessage(ChatColor.RED + "Please specify the type of weather");
-            }else{
-                p.performCommand("weather " + Arrays.toString(args));
-                p.sendMessage(ChatColor.YELLOW + "The weather has been set to " + Arrays.toString(args));
+                p.sendMessage(ChatColor.RED + "Please specify the type of weather (clear, rain, or thunder)");
+            }
+
+            World world = p.getWorld();
+            String weatherType = args[0].toLowerCase();
+
+            switch (weatherType) {
+                case "sun":
+                case "clear":
+                    world.setStorm(false); // Clear any existing weather
+                    world.setThundering(false);
+                    p.sendMessage(ChatColor.YELLOW + "The weather has been set to clear/sun");
+                    break;
+                case "rain":
+                    world.setStorm(true);
+                    p.sendMessage(ChatColor.YELLOW + "The weather has been set to rain");
+                    break;
+                case "storm":
+                case "thunder":
+                    world.setStorm(true);
+                    world.setThundering(true);
+                    p.sendMessage(ChatColor.YELLOW + "The weather has been set to thunder/storm");
             }
         }
         return true;
